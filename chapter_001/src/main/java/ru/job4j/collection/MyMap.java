1d00c3dcdaee3107b0340ddf557bd99831c23383
Kradeyes <yanin.r.a@mail.ru>
2020-08-04 15:19:01 +0300

    1. Создать модель User[999#322292]

--- chapter_001/src/main/java/ru/job4j/collection/MyMap.java
+++ chapter_001/src/main/java/ru/job4j/collection/MyMap.java
@@ -3,6 +3,9 @@ package ru.job4j.collection;
 import ru.job4j.generic.User;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyMap {
 
@@ -10,8 +13,22 @@ public class MyMap {
         private String name;
         private int children;
         private Calendar birthday;
+
+        public User(String name, int children, Calendar birthday) {
+            this.name = name;
+            this.children = children;
+            this.birthday = birthday;
+        }
     }
 
     public static void main(String[] args) {
+        User first = new User("Roman", 1,
+                new GregorianCalendar(1990, Calendar.AUGUST, 31));
+        User second = new User("Roman", 1,
+                new GregorianCalendar(1990, Calendar.AUGUST, 31));
+        Map<User, Object> testMap = new HashMap<>();
+        testMap.put(first, first);
+        testMap.put(second, second);
+        System.out.println(testMap);
+        }
     }
-}
