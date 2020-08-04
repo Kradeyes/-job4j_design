c1cbdf3133216135b902fcb5dea0987fecda3a0a
Kradeyes <yanin.r.a@mail.ru>
2020-08-04 15:06:56 +0300

    1. Создать модель User[999#322292]

---
+++ chapter_001/src/main/java/ru/job4j/collection/MyMap.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import ru.job4j.generic.User;
+
+import java.util.Calendar;
+
+public class MyMap {
+
+    public static class User {
+        private String name;
+        private int children;
+        private Calendar birthday;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
