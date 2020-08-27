dbf622c9b9480cc312f414696fd3801c1ed57a5a
Kradeyes <yanin.r.a@mail.ru>
2020-08-27 21:04:17 +0300

    1. Бот[7921#322335]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -14,14 +14,25 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str = in.readLine();
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     while (!(str.isEmpty())) {
                         System.out.println(str);
-                        if (str.contains("Bye")) {
+                        if (str.contains("Exit")) {
+                            out.write("Server closed\r\n".getBytes());
                             isWork = false;
                             break;
                         }
+                        if (str.contains("Hello")) {
+                            out.write("Hello, dear friend\r\n".getBytes());
+                        } else {
+                            if (str.contains("msg")) {
+                                String[] splitMsg = str.split(" ");
+                                String message = splitMsg[1].split("=")[1] + System.lineSeparator();
+                                out.write(message.getBytes());
+                            }
+                        }
+                        str = in.readLine();
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                 }
             }
         }
